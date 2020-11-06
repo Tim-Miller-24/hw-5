2025a09401b1e65e44db83b70c8088eb5c7f5635
Пользователь <erikruka26@gmail.com>
2020-11-06 13:42:12 +0500

    2hw done

--- index.js
+++ index.js
@@ -1,110 +1,49 @@
 
-   // Задание: (База пользователей)
-            
-var base = {
-    user: {
-        id: 1,
-    },
+// Задание: (База пользователей)
 
-    user: {
-        id: 2,
-    },
+let usersNum = +prompt('Введите количество пользователей');
 
-    user: {
-        id: 3,
-    },
+while ((usersNum == '') || isNaN(usersNum)) {
+    usersNum = +prompt('Введите количество пользователей');
 }
 
-for (base.user.id = 1; base.user.id <= 3; base.user.id++) {
-    base.user.name = prompt('Введите имя пользователя');
-    base.user.age = +prompt('Введите возраст пользователя');
-    console.log(`Пользователь: ${base.user.id}.
-Имя: ${base.user.name}.
-Возраст: ${base.user.age}.`);
+let usersBase = [{},]
+
+for (let i = 0; i < usersBase.length; i++) {
+    usersBase.push({
+        name: prompt('Введите Имя'),
+        age: +prompt('Введите Возраст'),
+    })
+
+    if (usersBase.length > usersNum) {
+        break;
+    }
 }
 
+usersBase.splice(0, 1)
+
+for (const key in usersBase) {
+    for (const x in usersBase[key]) {
+        console.log(`Объект: ${key}; ключ: ${x}; значение ключа: ${usersBase[key][x]}.`);
+    }
+}
 
 
 
 
 
 /*
-    Задание: (Корзина товаров)
-            Вам дается ссылка на js файл ее вы подключете перед вашим script.js:
-    -->
-            <script src="https://proweb.uz/javaScript/receipt.js"></script>
-           <!-- которая отдает function receipt() а эта функция отдает вам случайный объект
-            Например:
-            {
-                'Lavash': {
-                    info: 'Big Cheese',
-                    price: 15000
-                },
-                'Hot-dog': {
-                    info: 'mini',
-                    price: 8500
-                },
-                'Cola': {
-                    info: '1.5 L',
-                    price: 9000
-                }
-            }
-Вы должны при помощи цикла взять данные из этой ссылки и вывести конечную строку в console:
-    Где будет выведен список заказа и в конце общая стоимость заказа сразу c доставкой.
-    Должно выглядеть так
-    Вы заказали Lavash Big Cheese, Hot-dog mini, Cola 1.5 L,  | Общая стоимость 41500 сумм с доставкой (9000)
-     -->
-*/
+// Задание: (Корзина товаров)
+let payPrice = 0;
 
-
-// второе дз не получилось
-
-
-
-/* function receipt() {
-    const arrayReceipt = [
-    {
-        'Lavash': {
-            info: 'Big Cheese',
-            price: 15000
-        }, 'Hot-dog': {
-            info: 'mini',
-            price: 8500
-        }, 'Cola': {
-            info: '1.5 L',
-            price: 9000
-        }
-    },
-    {
-        'Milk': {
-            info: '1 L',
-            price: 8500
-        },
-        'Sugar': {
-            info: '1 kg',
-            price: 7000
-        }
-    },
-    {
-        'Cola': {
-            info: '1 L',
-            price: 6500
-        },
-        'Fanta': {
-            info: '1.5 L',
-            price: 9000
-        },
-        'Lipton': {
-            info: '1.2 L',
-            price: 5000
-        }, 
-        'Nestle': {
-            info: '10 L',
-            price: 8500
-        }
-    }]
-    const count = Math.floor(Math.random() * arrayReceipt.length)
-    return arrayReceipt[count]
+let order = receipt();
+for (const key in order) {
+    console.log(`Вы заказали - ${key}: ${order[key].info}`);
+    payPrice += order[key].price;
 }
 
-let pay  */
+console.log(`Общая стоимость: ${payPrice + 9000} сум. Включая доставку (9000 сум).`);
+
+ */
+
+
